d85af1aaa519eaf40191fe83b3e50a68abeac604
abdulloooh <abdullahakinwumi@gmail.com>
2020-11-13 23:04:38 +0100

    handled nicknames, login alert, and number of people online

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/static/index.html");
 });
 
+const people = [];
+
 io.on("connection", (socket) => {
   //connect event
-  const conn_msg = "A user connected";
-  socket.broadcast.emit("new user", conn_msg);
+
+  socket.emit("random", "Welcome to Laplace chat");
+  socket.emit("join", "What is your name?");
+
+  socket.on("new user", (name) => {
+    people.push({ [socket.id]: name });
+    socket.broadcast.emit("random", `${name} joined`);
+    io.emit("random", `${people.length} online`);
+  });
+
+  //typing
+  socket.on("typing", (msg) => socket.broadcast.emit("typing", msg));
+
+  // stop typing
 
   //receive message
   socket.on("sender message", (msg) => {
@@ -30,3 +44,4 @@ http.listen(PORT, () => console.log("Listening on port " + PORT));
 //auto exposes an endpoint /socket.io/socket.io.js
 //io.emit broadcast to every connected sockets
 //sender.broadcast.emit broadcast to every socket except the emitting socket
+//socket.emit to send to the emitting socket only
